13ab0375fa12e9e1d1b3d734d51f71912c3180d7
George Kwabena Asiedu <asiedug41@gmail.com>
2023-08-01 16:53:14 +0000

    update

--- index.js
+++ index.js
@@ -1,58 +1,37 @@
 const toggleTheme = document.querySelector('.toggle-theme')
 const body = document.querySelector('body')
-const header = document.querySelectorAll('h1')
-const headerText = document.querySelector('.header p')
-const countText = document.querySelectorAll('.count h3')
-const socials = document.querySelectorAll('#socials')
-const boxes = document.querySelectorAll('.box')
-const h2 = document.querySelectorAll('h2')
-const change = document.querySelectorAll('.change')
-const h5 = document.querySelectorAll('h5')
 const changeModeText = document.querySelector('.changeMode') 
+const card = document.querySelectorAll('#card')
+const header = document.querySelectorAll('.white')
+const pale = document.querySelectorAll('.pale')
 
-localStorage.setItem('mode', 'light')
-
-const getMode = localStorage.getItem('mode')
-
-if(getMode === 'light') {
-    body.classList.toggle('light')
-}
 
 toggleTheme.addEventListener('click', () => {
     toggleTheme.classList.toggle('active')
-        body.classList.toggle('dark')
-        toggleTheme.style.backgroundImage = 'linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))'
-        changeModeText.innerHTML = 'Dark Mode'
-        changeModeText.style.color = 'hsl(228, 34%, 66%)'
-
-        headerText.style.color = 'hsl(228, 34%, 66%)'
-
-        for(const social of socials) {
-            social.style.background = 'hsl(228, 28%, 20%)'
+    body.classList.toggle('dark')
+    toggleTheme.style.backgroundImage = 'linear-gradient(to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))'
+    changeModeText.innerHTML = 'Dark Mode'
+    changeModeText.style.color = 'hsl(228, 34%, 66%)'   
+    for(const cards of card) {
+        if(toggleTheme.classList.contains('active')) {
+            cards.style.background = 'hsl(228, 28%, 20%)'
+            for(const heading of header) {
+                heading.style.color = 'white'
+            }
+            for(const pales of pale) {
+                pales.style.color = 'hsl(228, 34%, 66%)'
+            }
+        } else {
+            body.classList.toggle('light')
+            toggleTheme.style.background = 'hsl(230, 22%, 74%)'
+            changeModeText.innerHTML = 'Light Mode'
+            cards.style.background = 'hsl(230, 63%, 93%)'
+            for(const heading of header) {
+                heading.style.color = 'hsl(230, 17%, 14%)'
+            }
+            for(const pales of pale) {
+                pales.style.color = 'hsl(228, 12%, 44%)'
+            } 
         }
-
-        for(changes of change) {
-            changes.style.color = 'hsl(228, 34%, 66%)'
-        }
-
-        for(const heading2 of h2) {
-            heading2.style.color = 'hsl(0, 0%, 100%)'
-        }
-
-        for(heading5 of h5) {
-            heading5.style.color = 'hsl(228, 34%, 66%)'
-        }
-
-        for(const heading of header) {
-            heading.style.color = 'hsl(0, 0%, 100%)'
-        }
-
-        
-        for(const box of boxes) {
-            box.style.background = 'hsl(228, 28%, 20%)'
-        }
-
-        for(const text of countText) {
-            text.style.color = 'hsl(0, 0%, 100%)'
-        }
+    }
 })
